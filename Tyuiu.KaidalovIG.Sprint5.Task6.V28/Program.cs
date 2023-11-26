61dab028968a62d73f88db5b72cb66a3de9da49d
Tyuiu.KaidalovIG@outlook.com <Kaydalov.2026@mail.ru>
2023-11-26 21:32:34 +0500

    Task6

---
+++ Tyuiu.KaidalovIG.Sprint5.Task6.V28/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KaidalovIG.Sprint5.Task6.V28.Lib;
+using System.IO;
+
+namespace Tyuiu.KaidalovIG.Sprint5.Task6.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт#5 |Выполнила: Кайдалов И. Г.|СМАРТ-Б-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт#5                                                                *");
+            Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Кайдалов Илья Григорьевич | СМАРТ-Б-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:DataSprint5InPutDataFileTask5V22.txt (файл взять из архива   *");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С:DataSprint5 и        *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть набор символьных данных. Найти   *");
+            Console.WriteLine("* количество четырехзначных чисел в заданной строке.                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask6V28.txt";
+            Console.WriteLine($"Данные находятся в файле: {path}");
+
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
